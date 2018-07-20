0a186d2018193bde377e68ab45e92562dc658d20
Henrique Alves <henri.falves@hotmail.com>
2018-07-20 03:01:02 -0300

    Atualização da model de transção, adicionando todos os campos

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,8 +1,19 @@
+using System;
+
 namespace StoneApi.Models {
     public class Transaction{
         public int TransactionId { get; set; }
         public double MerchantCnpj { get; set; }
         public int CheckoutCode { get; set; }
         public string CipheredCardNumber { get; set; }
+        public int AmountInCent { get; set; }
+        public int Installments { get; set; }
+        public string AcquirerName { get; set; }
+        public string PaymentMethod { get; set; }
+        public string CardBrandName { get; set; }
+        public string Status { get; set; }
+        public string StatusInfo { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime AcquirerAuthorizationDateTime { get; set; }
     }
 }
